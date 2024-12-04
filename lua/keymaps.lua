511667ea6e9cc0d00ce3aab77151d82e21f138ad
adnan-zahir <adnanzaidanz@gmail.com>
2024-12-04 12:51:41 +0700

    plugin: + neo-tree + nvim-tmux-navigation + catppuccin; config: + jk as quick escape + set colorscheme to catppuccin + disable netrw;

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,6 +31,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Quick escape form insert to normal mode
+vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Escape to normal mode' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -13,12 +13,65 @@ return {
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
+  init = function()
+    -- the remote file handling part
+    vim.api.nvim_create_autocmd('BufEnter', {
+      group = vim.api.nvim_create_augroup('RemoteFileInit', { clear = true }),
+      callback = function()
+        local f = vim.fn.expand '%:p'
+        for _, v in ipairs { 'dav', 'fetch', 'ftp', 'http', 'rcp', 'rsync', 'scp', 'sftp' } do
+          local p = v .. '://'
+          if f:sub(1, #p) == p then
+            vim.cmd [[
+              unlet g:loaded_netrw
+              unlet g:loaded_netrwPlugin
+              runtime! plugin/netrwPlugin.vim
+              silent Explore %
+            ]]
+            break
+          end
+        end
+        vim.api.nvim_clear_autocmds { group = 'RemoteFileInit' }
+      end,
+    })
+    vim.api.nvim_create_autocmd('BufEnter', {
+      group = vim.api.nvim_create_augroup('NeoTreeInit', { clear = true }),
+      callback = function()
+        local f = vim.fn.expand '%:p'
+        if vim.fn.isdirectory(f) ~= 0 then
+          vim.cmd('Neotree current dir=' .. f)
+          vim.api.nvim_clear_autocmds { group = 'NeoTreeInit' }
+        end
+      end,
+    })
+    -- keymaps
+  end,
   opts = {
+    sources = { 'filesystem', 'buffers', 'git_status', 'document_symbols' },
+    open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
     filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
+      hijack_netrw_behavior = 'open_current',
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+    },
+    window = {
+      mappings = {
+        ['\\'] = 'close_window',
+        ['<space>'] = 'none',
+        ['Y'] = function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          vim.fn.setreg('+', path, 'c')
+        end,
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = '',
+        expander_expanded = '',
+        expander_highlight = 'NeoTreeExpander',
       },
     },
   },
